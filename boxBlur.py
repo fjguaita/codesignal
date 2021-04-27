acc1bc12c0188063a69a998131f693c2d22577f8
fjguaita <fjguaita@outlook.es>
2021-04-27 19:05:24 -0300

    segundo commit main

--- boxBlur.py
+++ boxBlur.py
@@ -11,12 +11,12 @@ def boxBlur2(image):
     return [[(sum(sum(x[i:i+3]) for x in image[j:j+3])//9) for i in range(len(image[j])-2)] for j in range(len(image)-2)]
 
 
-image=[[36,0,18,9,9,45,27], 
+image=[[36,0,18,9,9,5,27], 
        [27,0,54,9,0,63,90], 
-       [81,63,72,45,18,27,0], 
-       [0,0,9,81,27,18,45], 
+       [81,63,72,45,5,27,0], 
+       [0,0,9,81,5,18,45], 
        [45,45,27,27,90,81,72], 
-       [45,18,9,0,9,18,45], 
+       [45,18,5,0,9,18,45], 
        [27,81,36,63,63,72,81]]
 
 
@@ -24,5 +24,5 @@ image=[[36,0,18,9,9,45,27],
 print(boxBlur(image))
 print(boxBlur2(image))
 
-
+# otros toquecitos en main
   
