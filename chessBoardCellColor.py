ea5325f2c8234d549717a9bd7eeec974d82155d2
fjguaita <fjguaita@outlook.es>
2021-04-27 22:30:27 -0300

    ultimo por hoy

--- chessBoardCellColor.py
+++ chessBoardCellColor.py
@@ -4,7 +4,18 @@ def chessBoardCellColor(c1, c2):
 
 
 
+def chessBoardCellColor2(cell1, cell2):
+    
+    return (ord(cell1[0])+int(cell1[1])+ord(cell2[0])+int(cell2[1]))%2==0
 
+
+
+def chessBoardCellColor3(cell1, cell2):
+    return (sum(map(ord,cell1+cell2)))%2 == 0
+
+
+cell1= "A1"
+cell2= "C3"
 
 # determinar si las casillas tienen el mismo color
 # tablero ajedrez
@@ -19,4 +30,3 @@ def chessBoardCellColor(c1, c2):
 #   A1
 
 print(chessBoardCellColor(cell1, cell2))
-   
